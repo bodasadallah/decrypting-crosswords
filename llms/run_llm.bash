c12507e9c930e083cdb6d5faf50c66f6714a84cc
Boda Sadallah <boda998@yahoo.com>
2023-12-01 11:56:16 +0400

    adding new dataset split

--- llms/run_llm.bash
+++ llms/run_llm.bash
@@ -12,19 +12,22 @@
 echo "starting......................."
 ###################### RUN LLM Eval ######################
 
-# --model_name='mistralai/Mistral-7B-v0.1' \
 # --model_name='meta-llama/Llama-2-7b-hf' \
+# --checkpoint_dir='experiments/Mistral-7B-v0.1/checkpoint-3000' \
 
 python evaluate_llm.py \
---checkpoint_dir='experiments/Mistral-7B-v0.1/checkpoint-5000' \
+--checkpoint_path='experiments/mistral-7b-v0.1_disjoint_2/checkpoint-1500' \
 --model_name='mistralai/Mistral-7B-v0.1' \
---batch_size=16 \
+--per_device_train_batch_size=32 \
 --num_examples=0 \
 --n_shots=0 \
---prompt='Below is a clue for a decrypting crossword. Your task is to solve this clue. The number of characters in the answer should be same as the number in the parenthesis. Just output the answer only.' \
---dataset_path='../data/naive_random.json' \
---save_file='mistral_5k_output_pred.txt' \
+--base_prompt='Below is a clue for a decrypting crossword. Your task is to solve this clue. The number of characters in the answer should be same as the number in the parenthesis. Just output the answer only.' \
+--save_file='mistral_disjoint2_1.5k_output_test_disjoint.txt' \
+--test_dataset_path='data/clue_json/guardian/word_initial_disjoint/test.json' \
+--old_dataset=1 \
+# --dataset_path='data/unique_targets' \
+# --test_dataset_path='data/unique_targets' \
 
 
-echo " ending "
+echo " ending " 
 #srun python run_clm.py config.json
