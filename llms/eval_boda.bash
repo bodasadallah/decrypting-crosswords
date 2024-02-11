ca690d376ae20069fa42acbbe7b8998a1870fda1
Boda Sadallah <boda998@yahoo.com>
2024-02-11 22:47:37 +0400

    prepare eval code

--- llms/eval_boda.bash
+++ llms/eval_boda.bash
@@ -14,47 +14,27 @@
 
 
 
-
-# echo "starting Evaluation......................."
-# ###################### RUN LLM Eval ######################
-# # --checkpoint_dir='experiments/Mistral-7B-v0.1/checkpoint-3000' \
-# # --model_name='meta-llama/Llama-2-7b-hf' \
-
-# python evaluate_llm.py \
-# --model_name='mistralai/Mistral-7B-v0.1' \
-# --per_device_train_batch_size=32 \
-# --num_examples=0 \
-# --n_shots=0 \
-# --test_dataset_path='boda/cryptonite' \
-# --save_file='mistral_cryptonite_1.5_cryptonite_test.txt' \
-# --dataset_type='cryptonite' \
-# --checkpoint_path='experiments/mistral_cryptonite_1.5k' \
-
-
-# # --test_dataset_path='data/clue_json/guardian/naive_random/test.json' \
-# # --test_dataset_path='data/clue_json/guardian/word_initial_disjoint/test.json' \
-
-
-# echo " ending " 
-
-################################ Save File Naming Convention:  MODELNAME_TRAIN-DATASET_TRAIN-STEPS_TEST-DATASET.txt
-
-
-
 echo "starting Evaluation......................."
 ###################### RUN LLM Eval ######################
-# --checkpoint_dir='experiments/Mistral-7B-v0.1/checkpoint-3000' \
+FULL_MODEL_NAME="meta-llama/Llama-2-7b-hf"
+# FULL_MODEL_NAME="mistralai/Mistral-7B-v0.1"
 
+TEST_DATASET="data/clue_json/guardian/word_initial_disjoint/val.json"
+DATASET_NAME="word_initial_disjoint"
+DATASET_TYPE="old"
+MODEL_NAME=$(echo $FULL_MODEL_NAME | cut -d "/" -f 2 | cut -d "-" -f 1)
+SAVE_DIR="new_experiments/$MODEL_NAME/$DATASET_NAME.txt"
+CHECKPOINT_PATH="/l/users/abdelrahman.sadallah/$MODEL_NAME/$DATASET_NAME/best_checkpoint"
 # --model_name='meta-llama/Llama-2-7b-hf' \
 
 python evaluate_llm.py \
---model_name='mistralai/Mistral-7B-v0.1' \
+--model_name=$FULL_MODEL_NAME \
 --per_device_train_batch_size=128 \
 --num_examples=0 \
 --n_shots=0 \
---save_file='mistral_disjoint_1k_disjoint_explicit_prompt.txt' \
---test_dataset_path='data/clue_json/guardian/word_initial_disjoint/test.json' \
---checkpoint_path='experiments/mistral-7b-v0.1_word_init_disjoint_new_prompt/checkpoint-1000' \
+--save_file=$SAVE_DIR \
+--test_dataset_path=$TEST_DATASET \
+--checkpoint_path=$CHECKPOINT_PATH \
 
 
 # --test_dataset_path='boda/cryptonite_filtered_testset' \
